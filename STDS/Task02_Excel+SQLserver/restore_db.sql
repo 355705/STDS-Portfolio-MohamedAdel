0b7a54663df2c222e175725af66097eaa5b5c421
Mohamed Adel <112807211+355705@users.noreply.github.com>
2025-12-11 18:36:29 +0200

    Delete STDS/Task02_Excel+SQLserver/restore_db.sql

--- STDS/Task02_Excel+SQLserver/restore_db.sql
+++
@@ -1,6 +0,0 @@
--- Active: 1760527093731@@localhost@1433@master
-RESTORE DATABASE AdventureWorks2019
-FROM DISK = N'/var/opt/mssql/code/AdventureWorks2019.bak'
-WITH MOVE 'AdventureWorks2019' TO '/var/opt/mssql/data/AdventureWorks2019.mdf',
-     MOVE 'AdventureWorks2019_log' TO '/var/opt/mssql/data/AdventureWorks2019_log.ldf',
-     REPLACE;
